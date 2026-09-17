a325a860cc6f8da67586bd189fdd4958f7fc00c9
Tong <28141@ud.ac.th>
2026-09-17 09:53:44 +0700

    Update Examination_4.py
    hour = int(input("จำนวนเต็ม: "))
    if hour <= 4:
       print("50 บาท")
    elif hour <= 2:
       print("20 บาท")
    else:
       print("0 บาท")

--- Examination_4.py
+++ Examination_4.py
@@ -1,7 +1,7 @@
 hour = int(input("จำนวนเต็ม: "))
-if hour <= 1:
-   print("0 บาท")
-elif hour >= 2:
+if hour <= 4:
+   print("50 บาท")
+elif hour <= 2:
    print("20 บาท")
 else:
-   print("50 บาท")
+   print("0 บาท")
